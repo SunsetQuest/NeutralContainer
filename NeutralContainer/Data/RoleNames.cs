402edaea114267ee6c9b0b85d462ecdb4070b4d8
Ryan Scott White <sunsetquest@hotmail.com>
2025-12-27 21:57:46 -0800

    Fix admin queue role attribute constant

--- NeutralContainer/Data/RoleNames.cs
+++ NeutralContainer/Data/RoleNames.cs
@@ -6,6 +6,7 @@ public static class RoleNames
     public const string Moderator = "Moderator";
     public const string Creator = "Creator";
     public const string Commenter = "Commenter";
+    public const string AdminAndModerator = Admin + "," + Moderator;
 
     public static readonly string[] All = { Admin, Moderator, Creator, Commenter };
     public static readonly string[] DefaultRoles = { Creator, Commenter };
